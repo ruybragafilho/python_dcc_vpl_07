b4e145a6fd487a4ce3ef4ec9802b4815516dd843
ruybragafilho <ruybragafilho@gmail.com>
2021-10-14 21:03:41 -0300

    Commit Inicial

---
+++ todo.py
@@ -0,0 +1,28 @@
+def lastNames(L):
+    """Mapeia uma lista de nomes para o ultimo nome de cada item.
+    Por exemplo, seja:
+    L = [['Antonio', 'Franco'], ['Caius', 'Vitelus'], ['Cristovao', 'Buarque']]
+    Entao lastNames(L) == ['Franco', 'Vitelus', 'Buarque']
+    """
+    return None
+
+def citations(L):
+    """Mapeia uma lista de nomes para a primeira letra mais sobrenome.
+    Por exemplo, seja:
+    L = [['Antonio', 'Franco'], ['Caius', 'Vitelus'], ['Cristovao', 'Buarque']]
+    entao citations(L) = ['A. Franco', 'C. Vitelus', 'C. Buarque']
+    Note que a primeira letra precisa estar capitalizada.
+    """
+    return None
+
+def fullCitations(L):
+    """Mapeia uma lista de nomes para as iniciais mais o ultimo nome.
+    Por exemplo, seja:
+    L = [
+        ['Antonio', 'Franco', 'Molina'],
+        ['Caius', 'vitelus', 'Aquarius'],
+        ['cristovao', 'buarque', 'Holanda']]
+    entao fullCitations(L) = ['A. F. Molina', 'C. V. Aquarius', 'C. B. Holanda']
+    Note que as iniciais precisam estar capitalizada.
+    """
+    return None
